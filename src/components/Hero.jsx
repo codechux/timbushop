48c396d5a1671570d5cc8583e9d11adce6619463
chux <chuxoguh@gmail.com>
2024-07-07 20:04:17 +0100

    dynamic hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import HeroImg from "../assets/heroTable-removebg-preview.png";
+import ProductDetailImg from "../assets/productdetail.png";
 
 const Hero = () => {
+  const location = useLocation();
+
+  const isProductDetailPage = location.pathname.includes("/product");
+
   return (
     <section className="h-[240px] bg-[#EDEDEF] flex items-center justify-between">
       <div>
-        <h2 className="text-4xl fon font-semibold pl-6">Product Catalog</h2>
-        <p className="pl-8">Home/Catalog</p>
+        <h2 className="text-4xl font-semibold pl-6">
+          {isProductDetailPage ? "Product Detail" : "Product Catalog"}
+        </h2>
+        <p className="pl-8">
+          Home/{isProductDetailPage ? "Product Detail" : "Catalog"}
+        </p>
       </div>
       <div>
-        <img src={HeroImg} alt="" />
+        <img
+          src={isProductDetailPage ? ProductDetailImg : HeroImg}
+          alt=""
+          className="h-full"
+        />
       </div>
     </section>
   );
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import ProductDetail from "../components/ProductDetail";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const ProductDetailPage = () => {
   const { id } = useParams();
   return (
     <div>
       <Header />
+      <Hero />
       <ProductDetail productId={id} />
     </div>
   );
